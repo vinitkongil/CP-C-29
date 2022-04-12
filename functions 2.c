0e848ac52cf7ce67cc1c91345d4a07ff8533b64d
vinitkongil <103025722+vinitkongil@users.noreply.github.com>
2022-04-12 12:03:02 +0530

    Add files via upload

---
+++ functions 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int add(int a,int b)
+{
+    int c = a + b;
+}
+int main()
+{
+    int sum;
+    sum=add(5,4);
+    printf("sum is %d\n",sum);
+    return 0;
+}
+
+
+OUTPUT
+
+sum is 9
