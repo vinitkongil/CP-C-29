6038f5952f38ab4f0349566e72ea26b8785b3ef6
vinitkongil <103025722+vinitkongil@users.noreply.github.com>
2022-04-12 12:25:04 +0530

    Add files via upload

---
+++ function 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int a=5;
+int b=2;
+void add()
+{
+    int sum;
+    sum= a + b;
+    printf("sum of a and b is %d\n",sum);
+}
+int main()
+{
+add();
+return 0;
+}
+
+OTYPUT--
+
+sum of a and b is 7
