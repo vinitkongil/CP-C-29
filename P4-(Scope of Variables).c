7b9ba52cf82f5059b64a25170740885240d5829b
vinitkongil <103025722+vinitkongil@users.noreply.github.com>
2022-05-09 19:32:12 +0530

    Add files via upload

---
+++ P4-(Scope of Variables).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int a=7;
+int b=4;
+void add()
+{
+    int sum;
+    sum = a + b;
+    printf("Sum of a and b is %d\n",sum);
+    
+}
+int main()
+{
+add();
+return 0;
+}
+
+
+Output-
+Sum of a and b is 11   
