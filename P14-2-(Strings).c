7f5bd96c776ce093930e099d1f38525f0951aa91
vinitkongil <103025722+vinitkongil@users.noreply.github.com>
2022-05-17 13:11:25 +0530

    Add files via upload

---
+++ P14-2-(Strings).c
@@ -0,0 +1,14 @@
+//This program is to show application of String//
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{ 
+    char strname [40]= "Vint Kongil";
+    printf ("The string is %s\n", strname);
+    return 0;
+}
+
+
+Output-
+The string is Vinit Kongil
