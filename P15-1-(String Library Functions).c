976095224910bd3779cd1e147c5b2350ca9eba08
vinitkongil <103025722+vinitkongil@users.noreply.github.com>
2022-05-31 11:54:45 +0530

    Add files via upload

---
+++ P15-1-(String Library Functions).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char arr[]="Vinit";
+    int len1 = strlen (arr);
+    printf ("string =%s\nlength=%d\n",arr,len1);
+    return 0;
+}
+
+output:-
+
+string =Vinit
+length=5
